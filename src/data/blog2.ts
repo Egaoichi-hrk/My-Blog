88e58c2f7126e712128045b87c87229bdbd9edd7
Egaoichi@hrk <harryskmt96@outlook.jp>
2025-07-02 02:23:33 +0900

    3commit

--- src/data/blog2.ts
+++ src/data/blog2.ts
@@ -18,13 +18,23 @@ export const blog2Post: BlogPost =
     title: '機械学習の基礎',
     date: '2025-06-02',
     excerpt: '学んだ機械学習の基礎をまとめていきます。',
-    imageUrl: '/progamingtimage.png',
+    imageUrl: '/library.jpg',
     author: 'Haruki',
     content: `
       目次：
       1.教師あり学習
       2.教師なし学習
-     
+     .
+     .
+     .
+     .
+     1. 教師あり学習
+    ・k-近傍法（k-NN）
+    ・サポートベクターマシン（SVM）
+    2.教師なし学習
+    ・k-meansクラスタリング
+    ・階層的クラスタリング
+    ・主成分分析（PCA）
 
     `
   }
